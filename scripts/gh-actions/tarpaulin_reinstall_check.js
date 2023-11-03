1b659e5b411961fc891d6fbfa2793b0615c6ee95
Steven Fontaine <steven.fontaine@abitoff.org>
2023-11-03 00:11:38 -0600

    fix build fail if no cache

--- scripts/gh-actions/tarpaulin_reinstall_check.js
+++ scripts/gh-actions/tarpaulin_reinstall_check.js
@@ -37,16 +37,19 @@ async function runCMD(cmd) {
 (async () => {
     // run the command to get the current version. look for something like "1.2.3" in stdout. if we
     // find something, assume that it's the current version. it'd be really dumb and annoying if it
-    // wasn't the current version for some reason...
-    const currentVersion = (await runCMD(CURRENT_VERSION_CMD))?.match(/\d+\.\d+\.\d+/)?.[0];
-    if (!currentVersion) {
-        // no match, error out
-        console.error(`Could not find tarpaulin's current version number in the output!`);
-        process.exit(1);
-    } else {
-        // match!
-        console.log(`Detected current version: ${currentVersion}`);
-    }
+    // wasn't the current version for some reason... if we get an error, it's likely that tarpaulin
+    // isn't installed, so we just fall through and do a force install.
+    let currentVersion;
+    try {
+        currentVersion = (await runCMD(CURRENT_VERSION_CMD))?.match(/\d+\.\d+\.\d+/)?.[0];
+        if (!currentVersion) {
+            // no version string, versions won't match, force install.
+            console.error(`Could not find tarpaulin's current version number.`);
+        } else {
+            // we found some kinda version string...
+            console.log(`Detected current version: ${currentVersion}`);
+        }
+    } catch (_) {}
 
     // if the versions don't match, force update. otherwise, there's nothing we need to do.
     if (currentVersion != REQUESTED_VERSION) {
